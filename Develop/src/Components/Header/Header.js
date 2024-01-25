2b080cc0b52ba06a4708337e3602f07f4b6ac559
TheWiseSailor <rudasilljay@gmail.com>
2024-01-24 22:27:22 -0500

    minor changes

--- Develop/src/Components/Header/Header.js
+++ Develop/src/Components/Header/Header.js
@@ -1,10 +1,14 @@
 // Header.js
 import React from 'react';
+import LogoImage from '../Header/HeaderImages/Logo.png';
+import "./Header.css"
+import NavLinks from '../NavLinks/NavLinks'; 
 
 const Header = () => {
   return (
-    <header>
-      <h1>Your Website Name</h1>
+    <header className='headerContainer'>
+      <img src={LogoImage} alt="JE" className='HeaderLogoImage' />
+      <NavLinks />
     </header>
   );
 }
