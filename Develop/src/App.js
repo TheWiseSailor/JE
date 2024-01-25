2c4011e64060613c0d0220f1c229873e06837e80
TheWiseSailor <rudasilljay@gmail.com>
2024-01-24 22:55:17 -0500

    simple css added

--- Develop/src/App.js
+++ Develop/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/Header/Header';
-import NavLinks from './Components/NavLinks/NavLinks';
+
 
 function App() {
   return (
