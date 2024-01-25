1954d835b3397b11db66c6d25bc5444adc8c3389
TheWiseSailor <rudasilljay@gmail.com>
2024-01-24 20:12:19 -0500

    basic wireframe for the header and navbar with no css

--- Develop/src/Components/NavLinks/NavLinks.js
+++ Develop/src/Components/NavLinks/NavLinks.js
@@ -1,10 +1,15 @@
-// NavbarLinks.js
+// NavLinks.js
 import React from 'react';
-
 
 const NavLinks = () => {
   return (
-
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 }
 
