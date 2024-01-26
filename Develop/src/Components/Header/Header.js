ba407185d6e69d4216ee45db2fd7ec56c4381e6f
TheWiseSailor <rudasilljay@gmail.com>
2024-01-25 22:24:11 -0500

    minor css stuff

--- Develop/src/Components/Header/Header.js
+++ Develop/src/Components/Header/Header.js
@@ -1,12 +1,31 @@
 // Header.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LogoImage from '../Header/HeaderImages/Logo.png';
 import "./Header.css"
 import NavLinks from '../NavLinks/NavLinks'; 
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='HeaderPrimary'>
+    <div className={`HeaderPrimary ${isSticky ? 'sticky' : ''}`}>
     <header className='headerContainer'>
 
       <img src={LogoImage} alt="JE" className='HeaderLogoImage' />
