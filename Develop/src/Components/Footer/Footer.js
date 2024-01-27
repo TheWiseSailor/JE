3116fded0062977ecf3710b29fc7728e7cbc25dd
TheWiseSailor <rudasilljay@gmail.com>
2024-01-27 13:59:37 -0500

    footer optimized

--- Develop/src/Components/Footer/Footer.js
+++ Develop/src/Components/Footer/Footer.js
@@ -1,15 +1,28 @@
-// Footer.js
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+      setShowFooter(isScrolledToBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <footer className="footer">
+    <footer className={`footer ${showFooter ? 'visible' : ''}`}>
       <div className="container">
         <div className="row">
-          <div className="col-lg-12 font-weight-bold">
-          Copyright © 2023 All rights reserved | JE.CO
+          <div className="col-lg-12">
+            <p className="font-weight-bold">Copyright © 2023 All rights reserved | JE.CO</p>
           </div>
         </div>
       </div>
