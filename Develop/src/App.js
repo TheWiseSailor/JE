9ec1e10a840bc1f3acb38014f12c0d8f7a95535c
TheWiseSailor <rudasilljay@gmail.com>
2024-01-27 13:01:54 -0500

    Simple Footer added

--- Develop/src/App.js
+++ Develop/src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
 import HomePage from './Components/Homepage/Hero/Hero';
 import AboutMe from './Components/Homepage/AboutMe/AboutMe';
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
 
       <HomePage /> 
       <AboutMe /> 
---
+++ Develop/src/Components/Footer/Footer.js
@@ -0,0 +1,20 @@
+// Footer.js
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 font-weight-bold">
+          Copyright © 2023 All rights reserved | JE.CO
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
