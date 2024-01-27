6a513d58fc7920cb6bc5b3caa5d0bce692971710
TheWiseSailor <rudasilljay@gmail.com>
2024-01-26 22:36:44 -0500

    bootstrap cdn added to the index file as well as populating the paragraph

--- Develop/src/Components/Homepage/AboutMe/AboutMe.js
+++ Develop/src/Components/Homepage/AboutMe/AboutMe.js
@@ -5,20 +5,18 @@ import './AboutMe.css';
 const AboutMeSection = () => {
   return (
     <div className='PrimaryContainer'>
-    <div className="about-me-container">
+      <div className="about-me-container">
   
-      <div className="JairImage1">
-        <img src={jairImage} alt="Jair" className='Jair'/>
-      </div>
-      <div className="description-container">
-       
-        <p className='AboutMeParagraph'>
-      
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet condimentum ex. 
-          Nullam vehicula, risus quis lacinia euismod, nisi urna faucibus mi, at euismod nunc justo 
-          et felis. Aliquam at augue ac ex convallis lacinia. 
-        </p>
-      </div>
+        <div className="JairImage1">
+          <img src={jairImage} alt="Jair" className='Jair'/>
+        </div>
+        <div className="description-container">
+          <p className='AboutMeParagraph font-weight-bold'> 
+            My name is Jair Escalona, I’m originally from Venezuela located in South America! Beautiful country.
+
+            I’m a Pastor, Professor and a Musician. I love to spend time making connections with people to guide them to Christ and also help them to use their abilities for His kingdom.
+          </p>
+        </div>
       </div>
     </div>
   );
