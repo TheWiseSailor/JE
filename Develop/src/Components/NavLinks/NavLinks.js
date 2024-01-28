fd43a286bee7687cda80746eecfec942f86c46ea
TheWiseSailor <rudasilljay@gmail.com>
2024-01-28 17:59:30 -0500

    pt2

--- Develop/src/Components/NavLinks/NavLinks.js
+++ Develop/src/Components/NavLinks/NavLinks.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import "./NavLinks.css";
-
+import AOSInitializer from "../AOS/AOSInitializer";
 const NavLinks = () => {
   return (
     <nav>
+          <AOSInitializer/>
+
       <ul>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/signup">SignUp</Link></li>
+        <li   data-aos="fade-up"
+              data-aos-duration="3000"><Link to="/products">Products</Link></li>
+        <li  data-aos="fade-down"
+              data-aos-duration="3000"><Link to="/contact">Contact</Link></li>
+        <li  data-aos="fade-up"
+              data-aos-duration="3000"><Link to="/signup">SignUp</Link></li>
       </ul>
     </nav>
   );
