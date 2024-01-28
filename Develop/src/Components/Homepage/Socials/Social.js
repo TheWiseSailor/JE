cab53c8137afd1cc0461d0d212fa9ac0dc36f491
TheWiseSailor <rudasilljay@gmail.com>
2024-01-28 16:31:41 -0500

    Social section made

--- Develop/src/Components/Homepage/Socials/Social.js
+++ Develop/src/Components/Homepage/Socials/Social.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './Social.css';
-import { FaWhatsapp, FaInstagram, FaFacebook, FaEnvelope } from 'react-icons/fa';
+import facebookIcon from './SocialImages/facebook.png'
+import mailIcon from './SocialImages/mail.png';
+import phoneIcon from './SocialImages/phone.png';
+import instagramIcon from './SocialImages/instagram.png';
+
 
 const Social = () => {
+  const handleCall = () => {
+    window.location.href = 'tel:+19802958480'; 
+};
     return (
         <div className="social-container">
-            <h2>Follow Us:</h2>
+          <div className='SocialContactContainer'>
+            <h2>Contact Me!</h2>
             <div className="social-icons">
-                <a href="https://whatsapp.com"><FaWhatsapp /></a>
-                <a href="https://instagram.com"><FaInstagram /></a>
-                <a href="https://facebook.com"><FaFacebook /></a>
-                <a href="mailto:example@example.com"><FaEnvelope /></a>
+                
+            <a href="https://www.instagram.com/jairescalona22/" target="_blank" rel="noopener noreferrer">
+                    <img src={instagramIcon} alt="Instagram" />
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100093069174673" target="_blank" rel="noopener noreferrer">
+            
+                    <img src={facebookIcon} alt="Facebook" />
+                </a>
+                <a href="mailto:jairescalona22@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <img src={mailIcon} alt="Email" />
+                </a>
+                <a href="tel:+19802958480" onClick={handleCall}>
+                    <img src={phoneIcon} alt="Phone" />
+                </a>
+                </div>
             </div>
         </div>
     );
