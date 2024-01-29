e5d5fe21f1ff866b055c69bd1b970d7dcc9b52ed
TheWiseSailor <rudasilljay@gmail.com>
2024-01-29 16:13:17 -0500

    ViewProducts

--- Develop/src/Components/Product/ViewProducts/ViewProducts.js
+++ Develop/src/Components/Product/ViewProducts/ViewProducts.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './ViewProducts.css';
 
 const ViewProducts = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
 
   const getProductDetails = (productId) => {
-
     return { id: productId, name: `Product ${productId}`, price: `$${productId * 10}`, imageUrl: `https://via.placeholder.com/150?text=Product+${productId}` };
   };
 
-  const product = getProductDetails(id); 
+  const product = getProductDetails(id);
 
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <img src={product.imageUrl} alt={product.name} />
-      <p>{product.price}</p>
+    <div className="ViewProductContainer"> 
+   
+      <img src={product.imageUrl} alt={product.name}  className='ViewProductImage'/>
+      <h1 className='ViewProductName'>{product.name}</h1>
+      <p className='ViewProductPrice'>{product.price}</p>
     </div>
   );
 };
